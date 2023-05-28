89e51701835ffc67518ad851ec55b90c39b4edf3
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 13:51:32 +0300

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,22 @@
+package week_02;
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word! ");
+
+        // test for me
+        /*
+        second test explain
+       */
+
+        //TODO next attend
+
+
+    }
+
+
+
+
+
+}
